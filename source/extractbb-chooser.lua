59c998acc6cc401523024a35a534f991d22a0d68
Max Chernoff <git@maxchernoff.ca>
2024-11-17 02:01:51 -0700

    Release v0.0.3

--- source/extractbb-chooser.lua
+++ source/extractbb-chooser.lua
@@ -7,7 +7,7 @@
 -- A wrapper script to allow you to choose which implementation of extractbb to
 -- use. Should hopefully be replaced with the ``scratch'' file in TeX Live 2025.
 --
--- v0.0.2 (2024-11-12) %%version %%dashdate
+-- v0.0.3 (2024-11-17) %%version %%dashdate
 
 ---------------------
 --- Configuration ---
